90ba63d2b6073d7d159ada68eda95aae7d352bd2
Ivan Carneiro <ivanexequielc@gmail.com>
2024-05-19 18:29:46 -0300

    first commit

---
+++ src/main/java/objetos_noche/cfp35/tp2_ivan/Tp2IvanApplication.java
@@ -0,0 +1,13 @@
+package objetos_noche.cfp35.tp2_ivan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2IvanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2IvanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/objetos_noche/cfp35/tp2_ivan/Tp2IvanApplicationTests.java
@@ -0,0 +1,13 @@
+package objetos_noche.cfp35.tp2_ivan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2IvanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
